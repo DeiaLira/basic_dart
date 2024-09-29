01c1e297627a9056a8dc0848e439c2146e14104d
Andreia Lira <andreia.s.lira08@gmail.com>
2024-09-29 19:40:52 -0300

    Adicionando código de repetição for

---
+++ estruturas_condicionais_e_de_repeticao/bin/laco_repeticao_for.dart
@@ -0,0 +1,11 @@
+//import 'package:hello_word/condicional_if.dart' as hello_word;
+
+//import 'dart:convert';
+//import 'dart:io';
+
+void main(List<String> arguments) {
+  String exemplo = "Meu primeiro exemplo";
+  for (var i = 0; i < exemplo.length; i++) {
+    print(exemplo[i]);
+  }
+}
