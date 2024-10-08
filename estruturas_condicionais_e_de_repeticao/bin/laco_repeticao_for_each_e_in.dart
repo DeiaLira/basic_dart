1ac603b3bd9b963e9a13679067a79099b14e23b2
Andreia Lira <andreia.s.lira08@gmail.com>
2024-10-08 11:55:13 -0300

    Laço de repetição for_in

---
+++ estruturas_condicionais_e_de_repeticao/bin/laco_repeticao_for_each_e_in.dart
@@ -0,0 +1,11 @@
+//import 'package:hello_word/condicional_if.dart' as hello_word;
+
+//import 'dart:convert';
+//import 'dart:io';
+
+void main(List<String> arguments) {
+  List exemplo = [1, 2, 3, 4, 5];
+  for (int letra in exemplo) {
+    print(letra);
+  }
+}
