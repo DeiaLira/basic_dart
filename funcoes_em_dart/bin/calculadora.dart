2cffe798591c324b56c21e845887a61aa4397f5e
Andreia Lira <andreia.s.lira08@gmail.com>
2024-10-09 17:22:22 -0300

    Alterando calculadora com uso de funções

---
+++ funcoes_em_dart/bin/calculadora.dart
@@ -0,0 +1,64 @@
+//import 'package:hello_word/condicional_if.dart' as hello_word;
+
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Digite o primeiro número:');
+  double operatingOne = double.parse(airConsole());
+
+  print('Digite o segundo número:');
+  double operatingTwo = double.parse(airConsole());
+
+  print('Digite a operação (+, -, *, /):');
+  var operation = stdin.readLineSync(encoding: utf8);
+
+  double result = 0;
+
+  switch (operation) {
+    case "+":
+      result = sum(operatingOne, operatingTwo);
+      print('Sua soma é: $result');
+      break;
+    case "-":
+      result = subtraction(operatingOne, operatingTwo);
+      print('Sua subtração é: $result');
+      break;
+    case "*":
+      result = multiplication(operatingOne, operatingTwo);
+      print('Sua multiplicação é: $result');
+      break;
+    case "/":
+      result = division(operatingOne, operatingTwo);
+      print('Sua divisão é: $result');
+      break;
+    default:
+      print('Operação inválida!');
+      exit(0);
+  }
+}
+
+String airConsole() {
+  var input = stdin.readLineSync(encoding: utf8);
+  return (input ?? "");
+}
+
+double sum(numberOne, numberTwo) {
+  double sum = numberOne + numberTwo;
+  return sum;
+}
+
+double subtraction(numberOne, numberTwo) {
+  double subtraction = numberOne - numberTwo;
+  return subtraction;
+}
+
+double multiplication(numberOne, numberTwo) {
+  double multiplication = numberOne * numberTwo;
+  return multiplication;
+}
+
+double division(numberOne, numberTwo) {
+  double division = numberOne / numberTwo;
+  return division;
+}
